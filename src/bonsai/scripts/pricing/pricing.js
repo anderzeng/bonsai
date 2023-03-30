16c359c076628474b3eb3e8aff8b47f2b88762bd
anderzeng <81279000+anderzeng@users.noreply.github.com>
2023-03-30 00:28:44 -0300

    Add style to pricing boxes on toggle activation

--- src/bonsai/scripts/pricing/pricing.js
+++ src/bonsai/scripts/pricing/pricing.js
@@ -1,7 +1,8 @@
 let activated = false;
-const toggleElement = document.getElementById('pricing--header-toggle-switch');
-
-console.log(toggleElement.firstChild);
+const toggleElement = document.getElementById('pricing-header-toggle-switch');
+const pricingBoxMonthly = document.getElementById('pricing-box-monthly');
+const pricingBoxYearly = document.getElementById('pricing-box-yearly');
+pricingBoxMonthly.classList.add('pricing__content-box-activate');
 
 function toggle() {
 	activated = !activated;
@@ -10,10 +11,14 @@ function toggle() {
 		toggleElement.firstElementChild.classList.add(
 			'pricing__header-toogle-switch-activation'
 		);
+		pricingBoxMonthly.classList.remove('pricing__content-box-activate');
+		pricingBoxYearly.classList.add('pricing__content-box-activate');
 	} else {
 		toggleElement.firstElementChild.classList.remove(
 			'pricing__header-toogle-switch-activation'
 		);
+		pricingBoxYearly.classList.remove('pricing__content-box-activate');
+		pricingBoxMonthly.classList.add('pricing__content-box-activate');
 	}
 }
 
