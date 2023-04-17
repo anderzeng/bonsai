7cfebfeae849bf0359d43748df850e95617f8c12
anderzeng <81279000+anderzeng@users.noreply.github.com>
2023-04-17 02:40:12 -0300

    Fix visibility toggler on FAQ section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-import faqItemToggleElements from './features/faq';
+import faqItemToggler from './features/faq';
 import activateToggler from './features/pricing';
 
 const App = {
@@ -6,12 +6,16 @@ const App = {
 		pricingSelectionToggler: document.querySelector(
 			'[data-id="plans-pricing-header-toggler"]'
 		),
-		faqItemToggleElements: faqItemToggleElements,
+		faqItemToggler: faqItemToggler,
 	},
 
 	init() {
+		App.registerEventListeners();
+	},
+
+	registerEventListeners() {
 		App.$.pricingSelectionToggler.addEventListener('click', activateToggler);
-		App.$.faqItemToggleElements();
+		App.$.faqItemToggler();
 	},
 };
 
