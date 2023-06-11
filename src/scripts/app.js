ff9e61ddd5e4e910d74b803bd33780872da8be63
Adrian Quesada Gómez <81279000+anderzeng@users.noreply.github.com>
2023-06-11 02:43:48 -0300

    Footer redesign and responsive improvements (#11)
    * Modify footer structure and styles
    Remove unnecessary styles
    
    * Squashed commit of the following:
    
    commit c52cc5c26e09f2c5090841974b5ede068d58c7a0
    Author: Adrian Quesada Gómez <81279000+anderzeng@users.noreply.github.com>
    Date:   Tue Jun 6 20:48:55 2023 -0300
    
        Dependencies update (#10)
    
        * Modify footer structure and styles
        Remove unnecessary styles
    
        * Update Vite to 4.3.9
        Update ESLint to 8.42.0
    
    commit e54750b1c613e21bf2b778136ce40d08841e12dd
    Author: Adrian Quesada Gómez <81279000+anderzeng@users.noreply.github.com>
    Date:   Wed May 3 14:14:15 2023 -0300
    
        Modify footer structure and styles (#8)
    
        Remove unnecessary styles
    
    * Fix responsive design on medium screen sizes
    Footer redesign to include personal logo
    Improve images and add resolutions
    Remove unused code related to media queries

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,7 +7,7 @@ const App = {
 		const view = new View();
 		view.bindEventPricingSelectionToggler(activatePricingToggler);
 		view.bindEventFaqItemVisibilityToggler();
-		if (window.innerWidth <= 768) {
+		if (window.innerWidth <= 640) {
 			view.applyInteractionOnSmallScreen();
 			view.bindEventHamburgerMenuButton(activateHamburgerMenu);
 			view.bindEventNavigationMenuSmallScreen();
