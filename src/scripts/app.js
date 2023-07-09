93dc7fac7c6bef22bf0f762f6270a3e6a65bcb30
anderzeng <81279000+anderzeng@users.noreply.github.com>
2023-07-09 19:00:45 -0300

    Apply Prettier 3.0 formatter

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,17 +3,17 @@ import activateHamburgerMenu from './layout/small-screen-hamburger-menu';
 import View from './view';
 
 const App = {
-	init() {
-		const view = new View();
-		view.bindEventPricingSelectionToggler(activatePricingToggler);
-		view.bindEventFaqItemVisibilityToggler();
-		if (window.innerWidth <= 640) {
-			view.applyInteractionOnSmallScreen();
-			view.bindEventHamburgerMenuButton(activateHamburgerMenu);
-			view.bindEventNavigationMenuSmallScreen();
-		}
-		view.setCurrentYear();
-	},
+  init() {
+    const view = new View();
+    view.bindEventPricingSelectionToggler(activatePricingToggler);
+    view.bindEventFaqItemVisibilityToggler();
+    if (window.innerWidth <= 640) {
+      view.applyInteractionOnSmallScreen();
+      view.bindEventHamburgerMenuButton(activateHamburgerMenu);
+      view.bindEventNavigationMenuSmallScreen();
+    }
+    view.setCurrentYear();
+  },
 };
 
 window.addEventListener('load', App.init);
--- src/scripts/layout/small-screen-hamburger-menu.js
+++ src/scripts/layout/small-screen-hamburger-menu.js
@@ -1,38 +1,38 @@
 export default function activateHamburgerMenu() {
-	const hamburgerMenuButton = document.querySelector(
-		'[data-id="small-screen-page-header-hamburger-menu-button"]'
-	);
+  const hamburgerMenuButton = document.querySelector(
+    '[data-id="small-screen-page-header-hamburger-menu-button"]'
+  );
 
-	toggleOpacity(hamburgerMenuButton.children[1]);
+  toggleOpacity(hamburgerMenuButton.children[1]);
 
-	const pageHeaderNavigationMenu = document.querySelector(
-		'[data-id="small-screen-page-header-navigation-menu"]'
-	);
+  const pageHeaderNavigationMenu = document.querySelector(
+    '[data-id="small-screen-page-header-navigation-menu"]'
+  );
 
-	toggleDisplay(pageHeaderNavigationMenu);
-	applyAnimationToHamburgerMenuButton(hamburgerMenuButton);
+  toggleDisplay(pageHeaderNavigationMenu);
+  applyAnimationToHamburgerMenuButton(hamburgerMenuButton);
 }
 
 function toggleOpacity(element) {
-	element.style.opacity === '0'
-		? (element.style.opacity = '1')
-		: (element.style.opacity = '0');
+  element.style.opacity === '0'
+    ? (element.style.opacity = '1')
+    : (element.style.opacity = '0');
 }
 
 function toggleDisplay(element) {
-	element.style.display === 'block'
-		? (element.style.display = 'none')
-		: (element.style.display = 'block');
+  element.style.display === 'block'
+    ? (element.style.display = 'none')
+    : (element.style.display = 'block');
 }
 
 function applyAnimationToHamburgerMenuButton(hamburgerMenuButton) {
-	const hamburgerMenuTopBar = hamburgerMenuButton.children[0];
-	const hamburgerMenuBottomBar = hamburgerMenuButton.children[2];
-	hamburgerMenuTopBar.classList.toggle(
-		'small-screen-page-header__hamburger-menu-top-bar-rotate'
-	);
+  const hamburgerMenuTopBar = hamburgerMenuButton.children[0];
+  const hamburgerMenuBottomBar = hamburgerMenuButton.children[2];
+  hamburgerMenuTopBar.classList.toggle(
+    'small-screen-page-header__hamburger-menu-top-bar-rotate'
+  );
 
-	hamburgerMenuBottomBar.classList.toggle(
-		'small-screen-page-header__hamburger-menu-bottom-bar-rotate'
-	);
+  hamburgerMenuBottomBar.classList.toggle(
+    'small-screen-page-header__hamburger-menu-bottom-bar-rotate'
+  );
 }
